3017ffef74c8d6f3bc21d2502d4cf993726f210f
hecht-a <axel.hct@gmail.com>
2023-08-04 11:08:21 +0200

    fix: remove plugin that run only failed tests

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -7,7 +7,7 @@
 
 import type { Config } from '@japa/runner'
 import TestUtils from '@ioc:Adonis/Core/TestUtils'
-import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-adonis'
+import { assert, specReporter, apiClient } from '@japa/preset-adonis'
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ import { assert, runFailedTests, specReporter, apiClient } from '@japa/preset-ad
 | Feel free to remove existing plugins or add more.
 |
 */
-export const plugins: Required<Config>['plugins'] = [assert(), runFailedTests(), apiClient()]
+export const plugins: Required<Config>['plugins'] = [assert(), apiClient()]
 
 /*
 |--------------------------------------------------------------------------
