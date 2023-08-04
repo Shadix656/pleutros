58111bbc2276c695e038e509343c1f47d36b7f1c
hecht-a <axel.hct@gmail.com>
2023-08-04 10:36:04 +0200

    feat: add Bde factory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,10 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from '@ioc:Adonis/Lucid/Factory'
+import Bde from 'App/Models/Bde'
+
+export const BdeFactory = Factory.define(Bde, ({ faker }) => {
+  return {
+    name: faker.company.name(),
+    city: faker.location.city(),
+    school: faker.company.name(),
+  }
+}).build()
